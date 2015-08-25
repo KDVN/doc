166674b4413f1cb1364c2bddafafa75667dfac87
Vu Thu Ha <Vu Thu Ha@VTHa-PC.kinden.com.vn>
2015-08-25 10:05:40 +0700

    Sua lai Copyright

--- conf.py
+++ conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'kdvn'
-copyright = u'2015, havt'
-author = u'havt'
+copyright = u'2015, KDERP IT-Dev. Team'
+author = u'KDERP IT-Dev. Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -225,7 +225,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'kdvn.tex', u'kdvn Documentation',
-   u'havt', 'manual'),
+   u'KDERP IT-Dev. Team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
